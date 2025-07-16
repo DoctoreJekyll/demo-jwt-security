f39567122eab74e3f729c058ea74633d942f6d38
Joseajierro <jrmar0805@gmail.com>
2025-07-16 10:11:14 +0200

    Commit

--- src/main/java/org/generations/demojwtsecurity/DemoJwtSecurityApplication.java
+++ src/main/java/org/generations/demojwtsecurity/DemoJwtSecurityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoJwtSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoJwtSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJwtSecurityApplication.class, args);
+    }
 
 }
--- src/test/java/org/generations/demojwtsecurity/DemoJwtSecurityApplicationTests.java
+++ src/test/java/org/generations/demojwtsecurity/DemoJwtSecurityApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoJwtSecurityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
